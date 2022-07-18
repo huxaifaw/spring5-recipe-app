2c142826d69adf6b84e079b462f7badcc612af81
I2C\mwaseem01 <mwaseem01@i2cinc.com>
2022-07-18 18:55:26 +0500

    Added Project Lombok dependency and refactored the code. Added @SLF4J annotation for logging Used @Transactional annotation to map the events happening in a method to a single transaction.

--- src/main/java/zaifi/springframework/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/zaifi/springframework/spring5recipeapp/services/RecipeServiceImpl.java
@@ -1,5 +1,6 @@
 package zaifi.springframework.spring5recipeapp.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import zaifi.springframework.spring5recipeapp.domain.Recipe;
 import zaifi.springframework.spring5recipeapp.repositories.RecipeRepository;
@@ -7,6 +8,7 @@ import zaifi.springframework.spring5recipeapp.repositories.RecipeRepository;
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
@@ -17,6 +19,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service");
         Set<Recipe> recipes = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
         return recipes;
