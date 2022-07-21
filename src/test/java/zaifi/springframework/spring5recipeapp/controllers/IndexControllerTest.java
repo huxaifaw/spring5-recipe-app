f6ce289cfe4c0c0420e8b2d5eaae946d25c34d72
HUZAIFA <huzaifawaseem97@gmail.com>
2022-07-21 23:51:46 +0500

    Created Mockito Test for IndexController Using ArgumentCaptor

--- src/test/java/zaifi/springframework/spring5recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/zaifi/springframework/spring5recipeapp/controllers/IndexControllerTest.java
@@ -2,16 +2,19 @@ package zaifi.springframework.spring5recipeapp.controllers;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+import zaifi.springframework.spring5recipeapp.domain.Recipe;
 import zaifi.springframework.spring5recipeapp.services.RecipeService;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class IndexControllerTest {
     @Mock
@@ -30,10 +33,25 @@ public class IndexControllerTest {
 
     @Test
     public void getIndexPage() {
+
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1L);
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(2L);
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeSet.add(recipe1);
+        recipeSet.add(recipe2);
+
+        when(recipeService.getRecipes()).thenReturn(recipeSet);
+
         String viewName = indexController.getIndexPage(model);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
 
         assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> recipeSetController = argumentCaptor.getValue();
+        assertEquals(2, recipeSetController.size());
     }
 }
