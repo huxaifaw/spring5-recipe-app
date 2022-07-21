ce1b7b6e5129eddfe272defdc6f97aee9455940b
HUZAIFA <huzaifawaseem97@gmail.com>
2022-07-22 00:46:09 +0500

    Running Tests using Spring Context and also Spring Data JPA @DirtiesContext annotation - read AUTHORS.txt...

---
+++ src/test/java/zaifi/springframework/spring5recipeapp/repositories/UnitOfMeasureRepositoryTest.java
@@ -0,0 +1,37 @@
+package zaifi.springframework.spring5recipeapp.repositories;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import zaifi.springframework.spring5recipeapp.domain.UnitOfMeasure;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryTest {
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    //@DirtiesContext
+    public void findByDescription1() {
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", unitOfMeasure.get().getDescription());
+    }
+
+    @Test
+    public void findByDescription2() {
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals("Cup", unitOfMeasure.get().getDescription());
+    }
+}
